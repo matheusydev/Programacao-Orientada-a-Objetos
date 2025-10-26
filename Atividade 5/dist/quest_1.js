f922717f8f1f75013fb8a99f35626bf0c1a7a49a
Matheusydev <matheusylanaraujo@gmail.com>
2025-10-26 17:18:54 -0300

    feat: primeira questão feita

---
+++ Atividade 5/dist/quest_1.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class Cliente {
+    id;
+    nome;
+    cpf;
+    dataNascimento;
+    contas;
+}
---
+++ Atividade 5/src/quest_1.ts
@@ -0,0 +1,7 @@
+class Cliente {
+    id: number;
+    nome: string;
+    cpf: string;
+    dataNascimento: Date;
+    contas: any[];
+}
